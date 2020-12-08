9dace364dce1a9a211cb51b7c54e467cd6cb0c95
Matthew Ost <most1@jhu.edu>
2020-12-08 18:43:32 -0500

    implemented data augmentation for nouns, verbs, adjectives with dumb replacement

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,12 @@
+import copy
 import pandas as pd
 import re
 
+import nltk
+from nltk.corpus import wordnet as wn
 from nltk.tokenize import wordpunct_tokenize
+
+from tqdm import tqdm
 
 class Data:
     """
@@ -16,6 +21,7 @@ class Data:
         self.label_col = label_col
         self.raw_tweets, self.labels = self._load_data(filepath, preprocess)
         self.tweets = None
+        self.augmentations = 0
 
         # Pre-process tweets
         if preprocess:
@@ -98,14 +104,81 @@ class Data:
             tweet = tweet.replace(e, self.escaped.get(e, "")) # replace with nothing for now. Maybe add this back later: f"<{e}>"
         return tweet
 
-    def _augment_data():
+    def augment_data(self):
         # TODO: Implement this
-        pass
+        new_tweets = []
+        new_labels = []
+        for tweet, label in tqdm(zip(self.tweets, self.labels), total=len(self.labels)):
+            results = self._augment_single_tweet(tweet)
+            new_tweets.extend(results)
+            new_labels.extend([label for _ in range(len(results))])
+            self.augmentations += len(results)
+
+        self.tweets.extend(new_tweets)
+        self.labels.extend(new_labels)
+        return # TODO: return something
+
+    def _candidate_to_replace(self, word, cand):
+        """Determine if a candidate is sufficient to replace the chosen word. Must be a distinct (non-homonym) unigram."""
+        distinct = (word not in cand[:cand.find(".")])
+        one_word = "_" not in cand
+        return distinct and one_word
+
+    def _candidate_to_be_replaced(self, token, tag):
+        """
+        Determine if a word is eligible to be replaced.
+        Must be a noun/adjective/verb, contain strictly letters, and be 3+ letters long.
+        """
+        noun = tag in ("NN", "NNS")
+        verb = tag in ("VB", "VBD", "VBG", "VBN", "VBP", "VBZ")
+        adjective = tag in ("JJ", "JJR", "JJS")
+        letters_only = token.isalpha()
+        length = len(token) > 2
+        return (noun or verb or adjective) and letters_only and length
+
+    def _augment_single_tweet(self, tweet):
+        """
+        Augment a single data example by selecting and replacing 1 noun, if possible.
+        Helpful resource for NLTK POS tagging: https://pythonprogramming.net/part-of-speech-tagging-nltk-tutorial/
+        """
+        #print(tweet)
+        #print()
+        pos_map = {"NN": "n", "NNS": "n", "VB": "v", "VBD": "v", "VBG": "v", "VBN": "v", "VBP": "v", "VBZ": "v", "JJ": "a", "JJR": "a", "JJS": "a"}
+
+        pos_tags = nltk.pos_tag(tweet)
+        to_replace = [(i, token, tag) for i, (token, tag) in enumerate(pos_tags) if self._candidate_to_be_replaced(token, tag)]
+
+        if len(to_replace) == 0:
+            return []
+
+        # Find a synonym for each word (if possible)
+        new_data = []
+        for idx, word, tag in to_replace:
+            #print(idx, word, tag, pos_map[tag])
+
+            # Generate list of candidate replacements with WordNet and choose the first one
+            candidates = [l for l in wn.synsets(word, pos=pos_map[tag]) if self._candidate_to_replace(word, l.name())]
+            if len(candidates) > 0:
+                r = candidates[0].name() # TODO: make this step smarter with word embeddings
+                replacement = r[:r.find(".")]
+                #print(replacement)
+
+                # Make the replacement
+                new_tweet = copy.deepcopy(tweet)
+                new_tweet[idx] = replacement
+                #print(new_tweet)
+                #print()
+                new_data.append(new_tweet)
+            #else:
+                #print("No synonym found\n")
+
+        return new_data
+
 
 if __name__ == "__main__":
     DATAFILE = "./Data/twitter_hate.csv"
     D = Data(DATAFILE, preprocess=True)
-    print(len(D.tweets))
+    D.augment_data()
     #print(D.raw_tweets[0])
     #print(type(D.tweets[0]))
     # quit()
